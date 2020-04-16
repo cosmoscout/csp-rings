f5fdf99cc9b270007299265c8d303657332cbb1e
Simon Schneegans <code@simonschneegans.de>
2020-04-16 10:51:01 +0200

    :wrench: Use local logger

--- src/Plugin.cpp
+++ src/Plugin.cpp
@@ -10,6 +10,7 @@
 #include "../../../src/cs-core/SolarSystem.hpp"
 #include "../../../src/cs-utils/logger.hpp"
 #include "../../../src/cs-utils/utils.hpp"
+#include "logger.hpp"
 
 #include <VistaKernel/GraphicsManager/VistaSceneGraph.h>
 #include <VistaKernel/GraphicsManager/VistaTransformNode.h>
@@ -50,7 +51,7 @@ void from_json(const nlohmann::json& j, Plugin::Settings& o) {
 
 void Plugin::init() {
 
-  spdlog::info("Loading plugin...");
+  logger()->info("Loading plugin...");
 
   mPluginSettings = mAllSettings->mPlugins.at("csp-rings");
 
@@ -80,13 +81,13 @@ void Plugin::init() {
     mRings.push_back(ring);
   }
 
-  spdlog::info("Loading done.");
+  logger()->info("Loading done.");
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 void Plugin::deInit() {
-  spdlog::info("Unloading plugin...");
+  logger()->info("Unloading plugin...");
 
   for (auto const& ring : mRings) {
     mSolarSystem->unregisterAnchor(ring);
@@ -96,7 +97,7 @@ void Plugin::deInit() {
     mSceneGraph->GetRoot()->DisconnectChild(ringNode.get());
   }
 
-  spdlog::info("Unloading done.");
+  logger()->info("Unloading done.");
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
